8a256cf013bb4c4e9fae65d0c1a50bb1e6c9e431
hyunjkluz <hyunjk.luz@gmail.com>
2019-01-04 14:42:04 +0900

    update shortner api

--- URLShortner/routes/shortner.js
+++ URLShortner/routes/shortner.js
@@ -3,25 +3,27 @@ const router = express.Router();
 const db = require('../module/pool');
 
 router.get('/', async (req, res) => {
-    let inputUrl = req.params.inputUrl;
+    let inputUrl = req.query.inputUrl;
     let shortenUrl = "";
 
-    let selectUrlQuery = 'SELECT * FROM urlDict WHERE original = ?';
+    let selectUrlQuery = "SELECT * FROM urlDict WHERE original = ?";
     let selectUrl = await db.queryParam_Arr(selectUrlQuery, [inputUrl]);
+    let lastId = 0;
 
-    if (selectUrl != null) {
+    if (selectUrl.length != 0) {
         shortenUrl = await encode(selectUrl[0].id);
     } else {
-        let lastId = await db.queryParam_None('SELECT MAX(id) AS lastId FROM urlDict');
+        lastId = await db.queryParam_None('SELECT MAX(id) AS lastId FROM urlDict');
         lastId = lastId[0].lastId;
+        shortenUrl = await encode(lastId + 1);
 
-        let insertUrlQuery = 'INSERT INTO '
-
-        res.status(200).send({
-            "encodedUrl": "localhost://" + shortenUrl
-        });
-
+        let insertUrlQuery = 'INSERT INTO urlDict(id, url, original) VALUES ( ?, ?, ?)';
+        let insertUrl = await db.queryParam_Arr(insertUrlQuery, [lastId + 1,shortenUrl, inputUrl]);
     }
+
+    res.status(200).send({
+        "encodedUrl": "localhost:3000/" + shortenUrl
+    });
 });
 
 function encode(id) {
