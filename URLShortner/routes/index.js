ac9acff8336435d091f25d91cc1fa75a12e380e7
hyunjkluz <hyunjk.luz@gmail.com>
2019-01-06 18:53:49 +0900

    update project

--- URLShortner/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.use('/shortner', require('./shortner.js'));
-router.use('/', require('./mainView.js'));
-
-
-module.exports = router;
